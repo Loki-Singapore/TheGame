a27a06997ee1695009357b5b961a9ea5ebfbc8ab
Loki-Singapore <Loki-Singapore@users.noreply.github.com>
2026-06-30 08:55:45 +0000

    Revert commits e80d244..e80d244
    This reverts 1 commit(s) from e80d244 to e80d244.
    
    Reverted commits:
    e80d244 feat: 状态面板接入Markdown渲染组件(compose-richtext)
    
    Co-authored-by: traeagent <traeagent@users.noreply.github.com>

--- textgame/app/build.gradle.kts
+++ textgame/app/build.gradle.kts
@@ -84,9 +84,6 @@ dependencies {
 
     implementation("androidx.datastore:datastore-preferences:1.0.0")
 
-    implementation("com.halilibo.compose-richtext:richtext-commonmark:0.17.0")
-    implementation("com.halilibo.compose-richtext:richtext-ui-material3:0.17.0")
-
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
